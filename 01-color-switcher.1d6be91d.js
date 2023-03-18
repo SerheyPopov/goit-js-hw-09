dedd1b162022965f840b4b3a1893b74be05e72a3
SerheyPopov <109915610+SerheyPopov@users.noreply.github.com>
2023-03-18 11:49:41 +0000

    Deploying to gh-pages from @ SerheyPopov/goit-js-hw-09@0e195db33cdbd1abbd2888d0664546a76fb9128f 🚀

--- 01-color-switcher.1d6be91d.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,intervalId=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(intervalId)})),t.stopBtn.disabled=!0;
-//# sourceMappingURL=01-color-switcher.1d6be91d.js.map
